e288e08fc9ee6ede148ec21953ce44c5d451d1a3
emilio-and24 <201183825+emilio-and24@users.noreply.github.com>
2025-03-11 11:41:52 -0600

    avance 3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 #
-car_data = pd.read_csv('../vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 #
 st.header("Analisis sobre anuncios de ventas de coches")
 #
